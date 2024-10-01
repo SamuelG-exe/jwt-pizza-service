c6549023d15b4a5daaea6874fd0aae59a59d6baf
Samuel Galbraith <samuelgalbraith.school@gmail.com>
2024-09-30 22:24:21 -0600

    finished Authrouter tests

--- src/routes/authRouter.test.js
+++ src/routes/authRouter.test.js
@@ -3,11 +3,13 @@ const app = require('../service');
 
 const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
 let testUserAuthToken;
+let testUserId;
 
 beforeAll(async () => {
   testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
   const registerRes = await request(app).post('/api/auth').send(testUser);
   testUserAuthToken = registerRes.body.token;
+  testUserId = registerRes.body.user.id;
   expect(testUserAuthToken).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
 });
 
@@ -37,6 +39,41 @@ test('login', async () => {
   expect(password).toBeTruthy;//Need to fix this test to be useful lol 
 
 });
+test('Update user', async () => {
+  const updateData = { email: 'updated@test.com', password: 'updatedPassword' };
+  const updateRes = await request(app)
+    .put(`/api/auth/${testUserId}`) 
+    .set('Authorization', `Bearer ${testUserAuthToken}`)
+    .send(updateData);
+
+  expect(updateRes.status).toBe(200);
+  expect(updateRes.body.email).toBe(updateData.email);
+});
+
+test('Update user Admin', async () => {
+  const adminUser = { name: 'Admin', email: 'admin@test.com', password: 'adminPassword', role: 'admin' };
+  const adminRegisterRes = await request(app).post('/api/auth').send(adminUser);
+  const adminAuthToken = adminRegisterRes.body.token;
+
+  const updateData = { email: 'updated@test.com', password: 'updatedPassword' };
+  const updateRes = await request(app)
+    .put(`/api/auth/${adminRegisterRes.body.user.id}`) 
+    .set('Authorization', `Bearer ${adminAuthToken}`)
+    .send(updateData);
+
+  expect(updateRes.status).toBe(200);
+  expect(updateRes.body.email).toBe(updateData.email);
+});
+
+test('Update user Fail, not your user', async () => {
+  const updateData = { email: 'updated2@test.com', password: 'updatedPassword2' };
+  const updateRes = await request(app)
+    .put(`/api/auth/${"notTheRightID"}`) 
+    .set('Authorization', `Bearer ${testUserAuthToken}`)
+    .send(updateData);
+
+  expect(updateRes.status).toBe(403);
+});
 
 test('logout', async () => {
   const logoutRes = await request(app).delete('/api/auth').set('Authorization', `Bearer ${testUserAuthToken}`).send();
@@ -52,4 +89,5 @@ test('Failed logout', async () => {
   expect(logoutRes.status).toBe(401);
   expect(logoutRes.body).toEqual({ message: 'unauthorized' });
 });
+
 
