4d9b304a817f61b0535d016fb3ca108301b24bb7
Samuel Galbraith <samuelgalbraith.school@gmail.com>
2024-12-03 15:11:30 -0700

    general latency

--- src/service.js
+++ src/service.js
@@ -11,7 +11,11 @@ const app = express();
 
 app.use(express.json());
 app.use(setAuthUser);
+
+const startTime = Date.now();
 app.use(metrics.requestTracker.bind(metrics));
+metrics.trackGeneralLatency(startTime); 
+
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', req.headers.origin || '*');
